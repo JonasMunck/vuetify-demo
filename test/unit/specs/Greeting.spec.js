49f164a933119645a0c699b3ce6cfc933dff77ae
Jonas Munck <jomun@danskebank.dk>
2018-03-12 14:58:00 +0100

    test that we say hi to user

--- test/unit/specs/Greeting.spec.js
+++ test/unit/specs/Greeting.spec.js
@@ -8,4 +8,11 @@ describe('Greeting.vue', () => {
     expect(vm.$el.textContent)
     .toContain('Please specify your username')
   })
+
+  it('should say hi to user', () => {
+    const Constructor = Vue.extend(Greeting)
+    const vm = new Constructor({propsData: {username: 'Jonas'}}).$mount()
+    expect(vm.$el.textContent)
+    .toContain('Hi Jonas, hope you have a nice day!')
+  })
 })
