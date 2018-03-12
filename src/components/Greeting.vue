d46d5b4ebe87419d226b3719c30292db12d73791
Jonas Munck <jomun@danskebank.dk>
2018-03-12 14:39:26 +0100

    add greeting component (but don't use it)

---
+++ src/components/Greeting.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="username !== ''">
+    Hi {{ username }}, hope you have a nice day!
+  </div>
+  <div v-else>
+    Please specify your username
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
